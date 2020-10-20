459b675284a052eb642abad233c9bc2b38aa007a
Svanazar <shashwats09@gmail.com>
2020-10-20 12:48:02 +0530

    added Select Input for status in "Add"

--- src/comp/add.js
+++ src/comp/add.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  List, Datagrid, TextField, ReferenceField, ImageField,
+  List, Datagrid, TextField, ReferenceField, ImageField, SelectField,
   EditButton, Edit, SimpleForm, TextInput, ReferenceInput, SelectInput, ImageInput,
   Create
 } from 'react-admin'
@@ -10,7 +10,11 @@ export const AddList = props => (
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <ImageField source="image" />
-            <TextField source="name" />
+            <ReferenceField label="Shop" source="shop_id" reference="Shop">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="shop_id" />
+            <TextField source="status" />
             <EditButton />
         </Datagrid>
     </List>
@@ -20,10 +24,15 @@ export const AddEdit = props => (
     <Edit {...props}>
         <SimpleForm>
             <TextInput disabled source="id" />
-            <TextInput source="name" />
             <ImageInput source="image" accept="image/*">
             	<ImageField source="src" />
             </ImageInput>
+            <TextInput source="shop_id" />
+            <SelectInput source="status" choices={[
+                {id: "Accepted", name: "Accepted"},
+                {id: "Pending", name: "Pending"},
+                {id: "Rejected", name: "Rejected"},
+            ]} />
         </SimpleForm>
     </Edit>
 );
@@ -32,10 +41,14 @@ export const AddCreate = props => (
     <Create {...props}>
         <SimpleForm>
             <TextInput source="id" />
-            <TextInput source="name" />
             <ImageInput source="image" accept="image/*">
             	<ImageField source="src" />
             </ImageInput>
+            <SelectInput source="status" choices={[
+                {id: "Accepted", name: "Accepted"},
+                {id: "Pending", name: "Pending"},
+                {id: "Rejected", name: "Rejected"},
+            ]} />
         </SimpleForm>
     </Create>
 );
